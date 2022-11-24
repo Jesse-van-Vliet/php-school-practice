0b79726e8398cea04fb79ad1ca6c3850136d9325
Jesse <9021012@student.zadkine.nl>
2022-11-24 13:44:40 +0100

    comit

---
+++ Opdracht_3_datatypes/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$string = "string nummer 1" . "<br>" ;
+$string2 = "string nummer 2" . "<br>";
+$num1 = 1 . "<br>";
+$num2 = 2 . "<br>"  ;
+
+echo $string;
+echo $string2;
+echo $num1;
+echo $num2;
+
+?>
+
+
+    
+</body>
+</html>
+
+
