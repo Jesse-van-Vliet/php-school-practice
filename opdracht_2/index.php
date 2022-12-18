1e3bd6cae0d96fc7557113cd9618643271361b98
Jesse <9021012@student.zadkine.nl>
2022-12-18 17:31:13 +0100

    opdracht 2

---
+++ opdracht_2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo "Hello world";
+
+// comment method 1.
+
+# comment method 2
+
+/* hey dit is 
+comment method 3 */
+
+
+
+// betekent dat er iets anders werd verwacht voordat de PHP-parser het einde van het script bereikte.
+
+?>
+    
+</body>
+</html>
